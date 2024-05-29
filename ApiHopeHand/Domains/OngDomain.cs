a71ec8c63e0f96d1b784780cede069ef1b7c66fe
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2024-05-29 16:46:16 -0300

    Reapply "Merge com o merge"
    This reverts commit 4f80c2f9f04c8d96ff992cd508e8eac253fd81ce.

---
+++ ApiHopeHand/Domains/OngDomain.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiHopeHand.Domains
+{
+    public class OngDomain
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Nome { get; set; }
+        public string? Foto { get; set; }
+        public string? Descricao { get; set; }
+        public string? EnderecoId { get; set; }
+
+        public int Cnpj {  get; set; }
+        public EnderecoDomain Endereco { get; set; }
+
+
+    }
+}
