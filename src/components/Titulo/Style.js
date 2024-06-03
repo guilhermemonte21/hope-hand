12952c530c2fd5c376cbebd3cc3fe3fba0dcb61b
HookCreeping <baptista2017henrique@gmail.com>
2024-06-03 17:27:08 -0300

    imagens adicionadas

---
+++ src/components/Titulo/Style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const TituloStyled = styled.Text`
+    color: #323030;
+    font-family: "Kanit_400Regular";
+`
--- src/components/Titulo/Titulo.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components/native";
-
-export const Titulo = styled.Text`
-    color: #323030;
-`
